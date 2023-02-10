6aa4ba647da671c7bc1f52b9327fbd56810593db
kleneway <kleneway@gmail.com>
2023-02-10 10:20:24 -0800

    removing boto to try to fix lambda issue

--- api/index.py
+++ api/index.py
@@ -1,13 +1,13 @@
-import os
+from os import environ
 from flask import Flask, request
 from langchain.llms import OpenAI
 from langchain.chains import VectorDBQA
-import pickle
-import boto3
-import faiss
+from pickle import load as pickle_load
+# from boto3 import client
+from faiss import read_index
 
 app = Flask(__name__)
-s3 = boto3.client("s3")
+# s3 = client("s3")
 
 @app.route('/')
 def home():
@@ -16,20 +16,20 @@ def home():
     # print("text: ", text)
     return "Hello World!"
 
-@app.route('/upload/<username>/<file_type>', methods=['POST'])
-def upload(username, file_type):
-    try:
-        file = request.files["file"]
-    except KeyError:
-        return "No file uploaded"
+# @app.route('/upload/<username>/<file_type>', methods=['POST'])
+# def upload(username, file_type):
+#     try:
+#         file = request.files["file"]
+#     except KeyError:
+#         return "No file uploaded"
     
-    if not username or not file_type:
-        return "Error: Invalid username or file type"
+#     if not username or not file_type:
+#         return "Error: Invalid username or file type"
 
-    bucket_name = os.environ.get("BUCKET_NAME")
-    s3.upload_fileobj(file, bucket_name, f"{username}/{file_type}/{file.filename}", ExtraArgs={"ACL": "public-read"})
+#     bucket_name = environ.get("BUCKET_NAME")
+#     s3.upload_fileobj(file, bucket_name, f"{username}/{file_type}/{file.filename}", ExtraArgs={"ACL": "public-read"})
 
-    return  f"https://{bucket_name}.s3.amazonaws.com/{username}/{file_type}/{file.filename}"
+#     return  f"https://{bucket_name}.s3.amazonaws.com/{username}/{file_type}/{file.filename}"
 
 @app.route('/ask/<username>', methods=['GET'])
 def ask(username):
@@ -39,11 +39,11 @@ def ask(username):
         return "No question provided"
 
     # Load the LangChain.
-    index = faiss.read_index(f"{username}_docs.index")
+    index = read_index(f"{username}_docs.index")
 
     # TODO: swap this out for a hosted vector store
     with open(f"{username}_faiss_store.pkl", "rb") as f:
-        store = pickle.load(f)
+        store = pickle_load(f)
 
     store.index = index
     chain = VectorDBQA.from_llm(llm=OpenAI(temperature=0), vectorstore=store)
